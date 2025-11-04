f8cf9b2c071eecde543d60b4fabfd4dd73178ce0
robot9493 <jaeyoon4726@gmail.com>
2025-11-04 09:01:38 -0800

    Deleted cluttered files

--- RSource/summary.R
+++ RSource/summary.R
@@ -88,4 +88,69 @@ generate_summary_files <- function(data, contvar, contvar_filt, catvar, output_p
   
   # Return the dataframes as a list to use in your R session
   return(list(cont = all_cont_summary, cat = cat_summary))
+  
+  
+  
+  
+  
+  
+  
+  
+  
+finaldf$Views <- factor(finaldf$Views) 
+
+table1::table1(~ViewsLog + Likes + Category + LTV + DiffGroup + LTVGroup
+, data = finaldf, na.rm = TRUE, digits = 2, format.number = TRUE)
+
+table1::table1(~ViewsLog + Likes + VideoLengthMin + LTV + CommentsGroup + DiffGroup + LTVGroup + VideoLengthGroup
+#find best option for digits = x
+table1::table1(~ViewsLog + Likes + Category + VideoLengthMin + VideoLengthGroup+ LTV + LTVGroup + DiffGroup + VideoLengthGroup
+, data = finaldf, na.rm = TRUE, digits = 5, format.number = TRUE)
+
+table1::table1(~ViewsLog + Likes + VideoLengthMin + LTV + DiffGroup + LTVGroup + VideoLengthGroup
+               | Category, data = finaldf, na.rm = TRUE, digits = 2, format.number = TRUE)
+
+table1::table1(~ViewsLog + Likes + Category + VideoLengthMin + LTV + CommentsGroup + LTVGroup + VideoLengthGroup
+table1::table1(~ViewsLog + Likes + Category + VideoLengthMin + LTV + LTVGroup + VideoLengthGroup
+               | DiffGroup, data = finaldf, na.rm = TRUE, digits = 2, format.number = TRUE)
+
+table1::table1(~ViewsLog + Likes + Category + VideoLengthMin + LTV + CommentsGroup + DiffGroup + LTVGroup
+table1::table1(~ViewsLog + Likes + Category + VideoLengthMin + LTV + DiffGroup + LTVGroup
+               | VideoLengthGroup, data = finaldf, na.rm = TRUE, digits = 2, format.number = TRUE)
+
+
+t1 <- table1::table1(~ViewsLog + Likes + Category + VideoLengthMin + LTV + CommentsGroup + DiffGroup + LTVGroup
+t1 <- table1::table1(~ViewsLog + Likes + Category + VideoLengthMin + LTV + DiffGroup + LTVGroup
+                     | VideoLengthGroup, data = finaldf, na.rm = TRUE, digits = 2, format.number = TRUE)
+
+save_html(t1, file = "C:/Users/jaeyo/Downloads/youtube-statistics/Tables/VideoLengthGroup.html")
+
+
+
+t1 <- table1::table1(~ViewsLog + Likes + Category + VideoLengthMin + LTV + CommentsGroup + LTVGroup + VideoLengthGroup
+               , data = finaldf, na.rm = TRUE, digits = 2, format.number = TRUE)
+t1 <- table1::table1(~ViewsLog + Likes + Category + VideoLengthMin + VideoLengthGroup+ LTV + LikesZero + LTVGroup + DiffGroup + VideoLengthGroup
+                     , data = finaldf, na.rm = TRUE, digits = 5, format.number = TRUE)
+
+table1::table1(~ViewsLog + Likes + VideoLengthMin + LTV + CommentsGroup + LTVGroup + VideoLengthGroup
+table1::table1(~ViewsLog + Likes + VideoLengthMin + LTV + LTVGroup + VideoLengthGroup
+               | Category, data = finaldf, na.rm = TRUE, digits = 2, format.number = TRUE)
+
+table1::table1(~ViewsLog + Likes + Category + VideoLengthMin + LTV + CommentsGroup + LTVGroup + VideoLengthGroup
+table1::table1(~ViewsLog + Likes + Category + VideoLengthMin + LTV + LTVGroup + VideoLengthGroup
+               | DiffGroup, data = finaldf, na.rm = TRUE, digits = 2, format.number = TRUE)
+
+table1::table1(~ViewsLog + Likes + Category + VideoLengthMin + LTV + CommentsGroup + LTVGroup
+table1::table1(~ViewsLog + Likes + Category + VideoLengthMin + LTV + LTVGroup
+               | VideoLengthGroup, data = finaldf, na.rm = TRUE, digits = 2, format.number = TRUE)
+
+save_html(t1, file = "C:/Users/jaeyo/Downloads/youtube-statistics/Tables/VideoLengthGroup.html")
+save_html(t1, file = "C:/Users/jaeyo/Downloads/youtube-statistics/Tables/total.html")
+
+
+names(finaldf)[names(finaldf) == "VideoLengthGroup"] <- "VideoLengthGroup"
+
+
+likesummary <- summary(finaldf$Likes)
+print(t1)
 }
